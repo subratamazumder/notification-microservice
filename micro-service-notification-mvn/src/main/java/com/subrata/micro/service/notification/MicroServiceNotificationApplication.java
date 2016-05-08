8f4c0c6c58fdb27d3b441aac1fbb483bcecf392d
subrata <subrata.besu@gmail.com>
2016-05-08 05:18:42 +0100

    push 3

--- micro-service-notification-mvn/src/main/java/com/subrata/micro/service/notification/MicroServiceNotificationApplication.java
+++ micro-service-notification-mvn/src/main/java/com/subrata/micro/service/notification/MicroServiceNotificationApplication.java
@@ -48,7 +48,7 @@ public class MicroServiceNotificationApplication implements CommandLineRunner {
 //		System.out.println();
 		
 		
-		System.out.println("Receiver thread started from git2......");
+		System.out.println("Receiver thread started from git3......");
 		queueService.receiveMessage();
 		
 		
